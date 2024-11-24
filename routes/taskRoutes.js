4ef0d21c85e40e896d0c9b3069305f9cd3e1e277
salfaidlt <salfaidabire@gmail.com>
2024-11-24 21:54:57 +0000

    adding new task OK

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -5,9 +5,15 @@ const router = express.Router();
 const verifyToken = require("../middlewares/authMiddleware")
 
 router.get("/", verifyToken,async (req, res) => {
-  // const tasks = await Task.find({ userId: decoded.id });
-  // res.json(tasks);
-  res.send("authenticated")
+  const tasks = await Task.find({ userId: decoded.id });
+  res.json(tasks);
 });
+
+router.post("/add", verifyToken, async(req, res) => {
+  const { userId, title, description, dueDate, status } = req.body
+  const task = new Task({ userId, title, description, dueDate, status })
+  await task.save()
+  res.status(201).json({ message: "Task created" })
+})
 
 module.exports = router;
