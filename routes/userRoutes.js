b0a7e83aad339465199399aedd33878382331557
salfaidlt <salfaidabire@gmail.com>
2024-12-20 23:43:48 +0000

    updated userRoutes file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,9 +17,7 @@ router.post("/login", async (req, res) => {
     const { email, password } = req.body
     const user = await User.findOne({ email })
     if (!user) {
-      return res.status(401).json({ 
-        error: 'Authentication failed'
-      });
+      return res.status(401).json({ error: 'Authentication failed' });
     }
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
